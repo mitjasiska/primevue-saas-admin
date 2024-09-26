8307b33f17cf19b01599016ed397a070e012d167
Mitja Šiška <mitja.siska@gmail.com>
2024-09-27 00:25:03 +0200

    Install and configure PrimeVue Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
 import './styles.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@/primevue/presets/aura'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +10,10 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Aura
+})
 
 app.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
+import tailwindcssPrimeui from 'tailwindcss-primeui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [tailwindcssPrimeui]
 }
-
