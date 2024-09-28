bf84655ef79c7c5225d07465a0aa727ba05713af
Mitja Šiška <mitja.siska@gmail.com>
2024-09-28 16:18:57 +0200

    Add layouts and refactor routes

---
+++ src/router/routes.ts
@@ -0,0 +1,33 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('@/layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import('@/views/HomePage.vue')
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/LoginPage.vue')
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/RegisterPage.vue')
+      }
+    ]
+  }
+];
+
+export default routes;
