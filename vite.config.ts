516e4386f59dc952c6c8127559582f9132bd2ba9
Mitja Šiška <mitja.siska@gmail.com>
2024-09-28 15:54:29 +0200

    Set build and esbuild targets to ES2022

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: 'ES2022'
+  },
+  esbuild: {
+    target: 'ES2022'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'ES2022'
+    }
   }
-})
+});
