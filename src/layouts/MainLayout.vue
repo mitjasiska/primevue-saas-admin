8775927538a4fbf70a5b52eb4ee03dbec831331d
Mitja Šiška <mitja.siska@gmail.com>
2024-10-10 21:31:58 +0200

    Add header, sidebar and footer to MainLayout.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,16 +3,40 @@ import AppLogo from '@/components/AppLogo.vue';
 </script>
 
 <template>
-  <header>
-    <div class="flex p-3">
-      <a href="/" class="shrink">
+  <div>
+    <header class="main-header fixed right-0 top-0 border-b border-black">
+      <div class="flex justify-between">
+        <div class="left">left</div>
+        <div class="right">right</div>
+      </div>
+    </header>
+    <div class="main-sidebar fixed flex-col border-r border-black lg:flex">
+      <div>
         <AppLogo />
-      </a>
+      </div>
+      <div class="min-h-screen">side content</div>
     </div>
-  </header>
-  <main>
-    <RouterView />
-  </main>
+    <div class="main-wrapper flex min-h-screen flex-col justify-between">
+      <main class="py-5">
+        <RouterView />
+      </main>
+      <footer class="border-t border-black">footer</footer>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-header {
+  left: var(--sidebar-width);
+  height: var(--header-height);
+}
+
+.main-sidebar {
+  width: var(--sidebar-width);
+}
+
+.main-wrapper {
+  padding-top: var(--header-height);
+  padding-left: var(--sidebar-width);
+}
+</style>
